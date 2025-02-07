2bb7003c444d537961073ac04e426372d7b3dc1d
zhangrj <rongjin.zh@gmail.com>
2025-02-08 02:19:45 +0800

    init docker added

---
+++ scripts/init_docker_fe.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt update
+
+apt install -y curl git vim wget
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+nvm install 20
+npm install next@latest react@latest react-dom@latest
+wget -qO- https://get.pnpm.io/install.sh | ENV="$HOME/.bashrc" SHELL="$(which bash)" bash -
