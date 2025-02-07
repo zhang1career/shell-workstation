df2eb1202c9b39dd4d5c5e60fff3881d774475b2
zhangrj <rongjin.zh@gmail.com>
2025-02-07 13:19:54 +0800

    fix bugs

--- .zshrc.d/aws.sh
+++ .zshrc.d/aws.sh
@@ -1,6 +1,6 @@
-aws_update_machine_info() {
+aws_update_machine_ip() {
 	aws ec2 describe-instances \
 		--filters Name=instance-state-name,Values=running \
-		--query 'Reservations[*].Instances[*].{name:Tags[?Key==`Name`]|[0].Value,sip:PrivateIpAddress,id:InstanceId,type:InstanceType}' \
-		--output text | sed -E 's/\t/,/g' >> ${DATA_PATH}/shell/machine_info.csv
+		--query 'Reservations[*].Instances[*].{name:Tags[?Key==`Name`]|[0].Value,sip:PrivateIpAddress}' \
+		--output text | sed -E 's/\t/,/g' >> ${DATA_HOME}/shell/machine_ip.csv
 }
--- .zshrc.d/jumper.sh
+++ .zshrc.d/jumper.sh
@@ -2,7 +2,7 @@ jump() {
 	# show hint when no parameter
 	if [ $# -le 0 ]; then
 		echo "$(font_yellow_bold Hint): Targets supported list as follow:"
-		map_keys /data/shell/machine_map.csv
+		map_keys ${DATA_HOME}/shell/machine_ip.csv
 		return 0
 	fi
 
@@ -15,12 +15,21 @@ jump() {
 	fi
 
 	TARGET=$1
-	
+	# query target user
+	USER=$(map_get ${DATA_HOME}/shell/machine_user.csv $TARGET)
+        if [ -z $USER ]; then
+                echo "$(font_red_bold ERROR): The $(font_bold $TARGET) user is not found."
+        fi
+	# query target password
+	CRED=$(map_get ${DATA_HOME}/shell/machine_cred.csv $TARGET)
+        if [ -z $CRED ]; then
+                echo "$(font_red_bold ERROR): The $(font_bold $TARGET) credential is not found."
+        fi
 	# query target address
-	ADDR=$(map_get /data/shell/machine_map.csv $TARGET)
+	ADDR=$(map_get ${DATA_HOME}/shell/machine_ip.csv $TARGET)
 	if [ -z $ADDR ]; then
 		echo "$(font_red_bold ERROR): The $(font_bold $TARGET) address is not found."
 	fi
 
-	$TARGET $ADDR
+	ssh -i $CRED ${USER}@${ADDR}
 }
