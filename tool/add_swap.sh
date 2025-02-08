f007f1f236c8bccd0fdd40436e8bc57bf0f58a4e
ec2-user <ec2-user@ip-172-31-1-83.ec2.internal>
2025-02-08 17:26:31 +0000

    fix add_swap

--- tool/add_swap.sh
+++ tool/add_swap.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # set swap size 2GB by default
-if [ $# -le 1 ]; then
-	SWAP_SIZE = 2
+if [ $# -lt 1 ]; then
+	SWAP_SIZE=2
 else
-	SWAP_SIZE = $1
+	SWAP_SIZE=$1
 fi
 
 
 # the swap file is n x 1 GB (128 MB x 8 x n):
-SWAP_COUNT = $((SWAP * 8))
+SWAP_COUNT=$((SWAP_SIZE * 8))
 sudo dd if=/dev/zero of=/swapfile bs=128M count=$SWAP_COUNT
 
 # updating file permission
